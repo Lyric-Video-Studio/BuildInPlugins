06e8b4ff7d2ec699fa247e672aa00d962f20ee2c
Matti Pulkkinen <pulkkinen@ultimatium.com>
2026-07-08 13:17:06 +0300

    Add nano banana 2 mini support

--- Google/ImageTrackPayload.cs
+++ Google/ImageTrackPayload.cs
@@ -4,8 +4,8 @@ namespace GooglePlugin
 {
     public class ImageTrackPayload : IPayloadPropertyVisibility
     {
-        [PropertyComboOptions(["gemini-3.1-flash-image-preview", "gemini-3-pro-image-preview", "gemini-2.5-flash-image", "models/imagen-4.0-generate-001"])]
-        public string Model { get; set; } = "gemini-3.1-flash-image-preview";
+        [PropertyComboOptions(["gemini-3.1-flash-lite-image", "gemini-3.1-flash-image-preview", "gemini-3-pro-image-preview", "gemini-2.5-flash-image", "models/imagen-4.0-generate-001"])]
+        public string Model { get; set; } = "gemini-3.1-flash-lite-image";
         public string Prompt { get; set; }
 
         [PropertyComboOptions(["1K", "2K", "4K"])]
