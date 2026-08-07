6387d662dc5f7bc3e3519a2220631a35eba747b8
Matti Pulkkinen <pulkkinen@ultimatium.com>
2026-08-07 07:02:01 +0300

    Change default minimax video model to H3

--- Minimax/TrackPayload.cs
+++ Minimax/TrackPayload.cs
@@ -12,7 +12,7 @@ namespace MinimaxPlugin
         private Request imgToVidPayload = new Request();
         private string videoModel;
         [Description("Video model")][TriggerReload]
-        public string VideoModel { get => videoModel ?? Settings?.model ?? "MiniMax-Hailuo-02"; set => videoModel = value; }
+        public string VideoModel { get => videoModel ?? Settings?.model ?? "MiniMax-H3"; set => videoModel = value; }
         [Description("Video settings")][IgnorePropertyName]
         public Request Settings { get => imgToVidPayload; set => imgToVidPayload = value; }
         [Description("MiniMax-H3 V2 settings")]
@@ -21,10 +21,10 @@ namespace MinimaxPlugin
         public SubjectRefContainer SubjectReferences { get; set; } = new();
         [Description("MiniMax-H3 reference inputs. Do not combine these with first or last frames.")]
         public H3ReferenceContainer H3References { get; set; } = new();
-        public void OnDeserialized() => videoModel ??= Settings?.model ?? "MiniMax-Hailuo-02";
+        public void OnDeserialized() => videoModel ??= Settings?.model ?? "MiniMax-H3";
         public bool ShouldPropertyBeVisible(string propertyName, object trackPayload, object itemPayload)
         {
-            var model = (trackPayload as TrackPayload)?.VideoModel ?? "MiniMax-Hailuo-02";
+            var model = (trackPayload as TrackPayload)?.VideoModel ?? "MiniMax-H3";
             if (propertyName == nameof(VideoModel)) return true;
             if (propertyName == nameof(Settings)) return model != "MiniMax-H3";
             if (propertyName == nameof(H3Settings) || propertyName == nameof(H3References)) return model == "MiniMax-H3";
